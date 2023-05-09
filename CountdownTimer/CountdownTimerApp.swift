34e8e15f4a13a6dd0cb81f4fcc00a58329d23681
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-05-09 08:14:34 -0500

    Initial Commit

---
+++ CountdownTimer/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  CountdownTimer
+//
+//  Created by Matthew RIley on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
